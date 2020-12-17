4550daa8452a2e51119c8e8f9ec39b35f97f38d3
Nicolas Chambrier <naholyr@gmail.com>
2020-12-17 20:42:01 +0100

    J2: fix mocha tests --watch with esm (refs mochajs/mocha#4374)

--- app/roti/test/dumb.js
+++ app/roti/test/dumb.js
@@ -1,12 +1,12 @@
 /* eslint-env mocha */
 
-import { expect } from "chai";
+import chai from "chai";
 
 describe("Dumb things", () => {
   it("Array should concat", () => {
     const a = [1, 2, 3];
     const b = [4, 5, 6];
     const c = a.concat(b);
-    expect(c).to.eql([1, 2, 3, 4, 5, 6]);
+    chai.expect(c).to.eql([1, 2, 3, 4, 5, 6]);
   });
 });
